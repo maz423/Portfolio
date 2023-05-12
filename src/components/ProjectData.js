4757da3d270755e46e26ac358be5ba32e80851a3
maz423 <maz423@usask.ca>
2023-05-12 02:13:06 -0600

    Updated Project GIT links

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -4,7 +4,7 @@ export const ProjectData = [
     {
       title: "Ecommerce - webapp",
       description: "The web app allows University students to buy or sell used books and Allows Book stores to list their books.\nThis is a Team-based project where I am responsible for designing and implementing the front end of the web application using React, Bootstrap, HTML, CSS, JavaScript..",
-      githubLink: "https://github.com/yourusername/project1",
+      githubLink: "https://github.com/maz423/Bookshop",
       youtubeLink: "https://youtube.com/watch?v=yourvideo1",
       date: "2021-05-01",
       pictures: [
@@ -19,7 +19,7 @@ export const ProjectData = [
     {
       title: "State diagram JavaFx",
       description: "In this project, I built a visual editor using JavaFX to create a state‐machine diagram editor with multiple synchronized views. The editor includes a tool palette that allows users to create state nodes, connect states with transition links, move and edit items in the workspace, and pan the view. A property panel on the right‐hand side of the screen enables users to edit details for a state node or a transition link, while pressing the Delete key removes the selected item and any dependent items. The project covered immediate‐mode graphics, interaction with graphics, more complex model‐view‐controller architectures, and demonstrated my skills in full‐stack development, algorithms, and software engineering.",
-      githubLink: "https://github.com/yourusername/project2",
+      githubLink: "https://github.com/maz423/StateDiagramApp",
       youtubeLink: "https://youtube.com/watch?v=yourvideo2",
       date: "2023-05-02",
       pictures: [
@@ -50,7 +50,7 @@ export const ProjectData = [
 
 
     {
-        title: "Target Trainer app",
+        title: "https://github.com/maz423/Target-Training-App",
         description: "Designed and developed a graphical editor in JavaFX to enable users to create and practice 2D targeting with custom targets. The editor includes features such as target creation, deletion, resizing, and selection, as well as undo/redo capabilities. Additionally, the editor allows for clipboard operations and cut/copy/paste functionalities. As part of the project, successfully deployed and implemented logging for the target practice sessions",
         githubLink: "https://drive.google.com/uc?export=view&id=1vKu7jWTQOZk79nJFLifgLRsbBKw7mSyF",
         youtubeLink: "https://youtube.com/watch?v=yourvideo2",
@@ -67,7 +67,7 @@ export const ProjectData = [
       {
         title: "REST vs SOAP app",
         description: "This project involved building two chatroom applications using different API technologies, REST and SOAP. The apps allow users to create chatrooms, list existing rooms, join and leave chatrooms, and display previously sent messages with a maximum delay of 1.5 seconds for new messages. The REST app was built using Flask and C# as the client, while the SOAP app was built using C# as the server and client. Both apps required Docker to set up and run. The main goal of this project was to conduct a profiling experiment that involved testing and comparing the performance of these two chatroom applications and determine which API technology was ideal to use in what scenario. It was concluded that Apps where performance is a major concern such as web browsing, social media, chat apps etc. REST seems to be a better fit for such purposes due to its light‐weight nature and faster response times, whereas SOAP shines in Security, Reliability and failure handling, and such systems where these factors are immensely important such as Financial, Payment Gateways, Telecommunication. SOAP would be a better fit. This project demonstrated proficiency in API technologies, client‐server architecture, containerization, Profiling.",
-        githubLink: "https://github.com/yourusername/project2",
+        githubLink: "https://github.com/maz423/ChatApp-REST-VS-SOAP-",
         youtubeLink: "https://youtube.com/watch?v=yourvideo2",
         date: "2023-05-02",
         pictures: ["https://drive.google.com/uc?export=view&id=1X0UBY5OQq5TZ-rsuwy7ogaOEWvU5btKx"
@@ -79,7 +79,7 @@ export const ProjectData = [
       {
         title: "CBO",
         description: "Designed a 3 tier Web application to help the organization in keeping track of their staff members and manage the records of their customers. This was a solo project where I worked on both the front and backend. Using React, JavaScript, HTML, MySQL, and Docker (Full-stack web development). ",
-        githubLink: "https://github.com/yourusername/project2",
+        githubLink: "https://github.com/maz423/CBO",
         youtubeLink: "https://youtube.com/watch?v=yourvideo2",
         date: "2023-05-02",
         pictures: [
@@ -95,7 +95,7 @@ export const ProjectData = [
       {
         title: "Chat application in Java using Sockets and TCP",
         description: "This project involved building two chatroom applications using different API technologies, REST and SOAP. The apps allow users to create chatrooms, list existing rooms, join and leave chatrooms, and display previously sent messages with a maximum delay of 1.5 seconds for new messages. The REST app was built using Flask and C# as the client, while the SOAP app was built using C# as the server and client. Both apps required Docker to set up and run. This project demonstrated proficiency in API technologies, client‐server architecture, and containerization.",
-        githubLink: "https://github.com/yourusername/project2",
+        githubLink: "https://github.com/maz423/Chatapp-Java",
         youtubeLink: "https://youtube.com/watch?v=yourvideo2",
         date: "2023-05-02",
         pictures: ["https://drive.google.com/uc?export=view&id=1EhI22-CRV2YiOndBjEXhIepeMWhahQr4",
@@ -111,7 +111,7 @@ export const ProjectData = [
       {
         title: "Portfolio",
         description: "Developed a professional portfolio from scratch utilizing React, Bootstrap, and JavaScript to showcase my projects and educational experience. This portfolio serves as a comprehensive representation of my skills, abilities, and accomplishments.",
-        githubLink: "https://github.com/yourusername/project2",
+        githubLink: "https://github.com/maz423/Portfolio",
         youtubeLink: "https://youtube.com/watch?v=yourvideo2",
         date: "2023-05-02",
         pictures: [
